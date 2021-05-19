f4e08c92df7bba176c164312976960f529c3e42e
Janzen Aguila <janzenaguila@gmail.com>
2021-05-19 22:04:33 +0800

    initial commit

---
+++ precache-manifest.a2df828513250729ab5749b7e058e3e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6bbe3473ca0c576ce362b063baeba7",
+    "url": "/textsimengineclient/index.html"
+  },
+  {
+    "revision": "25998ec8dd4ed6138cc8",
+    "url": "/textsimengineclient/static/css/main.b2b7f88f.chunk.css"
+  },
+  {
+    "revision": "9b9216fbb45c4e03121c",
+    "url": "/textsimengineclient/static/js/2.131ecc40.chunk.js"
+  },
+  {
+    "revision": "740989353e2da24dac6423009632c72f",
+    "url": "/textsimengineclient/static/js/2.131ecc40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25998ec8dd4ed6138cc8",
+    "url": "/textsimengineclient/static/js/main.2655fdd9.chunk.js"
+  },
+  {
+    "revision": "c2b8951ab9bbcc839ada",
+    "url": "/textsimengineclient/static/js/runtime-main.a1d979e2.js"
+  },
+  {
+    "revision": "343666efb95be952949666a37bd6a259",
+    "url": "/textsimengineclient/static/media/InitLogo.343666ef.png"
+  }
+]);
